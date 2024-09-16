cde6b1b008192c64ba72b504b85ec318b5109582
Ronak Pipalava <30079944@adani.com>
2024-09-16 14:39:58 +0530

    Added link and changed logo

--- src/constants/index.js
+++ src/constants/index.js
@@ -70,7 +70,7 @@ export const PROJECTS = [
 ];
 
 export const CONTACT = {
-  address: "767 Fifth Avenue, New York, NY 10153 ",
-  phoneNo: "+12 4555 666 00 ",
-  email: "me@example.com",
+  address: "Ahmedabad, Gujarat, India",
+  phoneNo: "+91 84695-11412 ",
+  email: "ronakpipalava.linkedin@gmail.com",
 };
