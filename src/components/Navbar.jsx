1cd6ac9b5babac229ef437c21a7202fc13a6ec2c
Ronak Pipalava <30079944@adani.com>
2024-09-16 14:39:58 +0530

    Tab Icon and Tab name changed

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,4 +1,4 @@
-import logo from "../assets/kevinRushLogo.png"
+import logo from "../assets/ronakPipalavaLogo.png"
 import { FaLinkedin } from "react-icons/fa"
 import { FaGithub } from "react-icons/fa"
 import { FaSquareXTwitter } from "react-icons/fa6"
@@ -7,13 +7,13 @@ import { FaInstagram } from "react-icons/fa"
 const Navbar = () => {
   return <nav className="mb-20 flex items-center justify-between py-6">
     <div className="flex flex-shrink-0 items-center">
-        <img className="mx-2 w-10" src={logo} alt="logo" />
+        <img className="mx-2 w-20" src={logo} alt="logo" />
     </div>
     <div className="m-8 flex items-center justify-center gap-4 text-2xl">
-        <FaLinkedin />
-        <FaGithub />
-        <FaInstagram />
-        <FaSquareXTwitter />
+        <a href="https://www.linkedin.com/in/ronak-pipalava/" target="_blank"> <FaLinkedin /></a>
+        <a href="https://github.com/Ronak347" target="_blank"> <FaGithub /></a>
+        <a href="https://ronakap-portfolio.vercel.app/" target="_blank"> <FaInstagram /></a>
+        <a href="https://ronakap-portfolio.vercel.app/" target="_blank"> <FaSquareXTwitter /></a>
     </div>
   </nav>
 }
